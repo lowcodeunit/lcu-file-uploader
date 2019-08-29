7d60cc9058519660c47ec7de461dd9063709e5c2
powhound4 <george.hatch@fathym.com>
2019-08-29 11:52:25 -0600

    extract files from fileUploader

--- projects/common/src/lib/controls/file-uploader/file-uploader.component.ts
+++ projects/common/src/lib/controls/file-uploader/file-uploader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
 
 @Component({
@@ -6,11 +6,37 @@ import { FileUploader } from 'ng2-file-upload';
   templateUrl: './file-uploader.component.html',
   styleUrls: ['./file-uploader.component.scss']
 })
+
 export class FileUploaderComponent implements OnInit {
-public FileUploader: FileUploader = new FileUploader({});
-  constructor() { }
+
+protected URL = '/api/';
+public FileUploader: FileUploader = new FileUploader({url: this.URL});
+
+public SelectedFiles: Array<File>;
+
+
+@Output('files-to-upload')
+  public FilesToUpload: EventEmitter<Array<File>>;
+
+  constructor() { 
+    this.SelectedFiles = new Array<File>();
+    this.FilesToUpload = new EventEmitter<Array<File>>();
+  }
 
   ngOnInit() {
   }
 
+  onFileChanged(event) {
+    console.log("event = ", event.queue);
+    if (this.SelectedFiles) {
+      for (let i = 0; i < event.queue.length; i++) {
+        console.log("pushing: ", event.queue[i].file.rawFile);
+        this.SelectedFiles.push(event.queue[i].file.rawFile);
+      }
+    }
+    console.log("file uploaded = ", this.SelectedFiles);
+    this.FilesToUpload.emit(this.SelectedFiles);
+  }
+
 }
+ 
