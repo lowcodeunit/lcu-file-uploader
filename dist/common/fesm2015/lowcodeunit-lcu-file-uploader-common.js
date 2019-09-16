cb4601156ea71365c9163339c43545ead7b2e4bd
powhound4 <george.hatch@fathym.com>
2019-09-16 15:54:11 -0600

    button styling

--- dist/common/fesm2015/lowcodeunit-lcu-file-uploader-common.js
+++ dist/common/fesm2015/lowcodeunit-lcu-file-uploader-common.js
@@ -45,8 +45,8 @@ class FileUploaderComponent {
 FileUploaderComponent.decorators = [
     { type: Component, args: [{
                 selector: 'lcu-file-uploader',
-                template: "<input  style=\"display: none\" type=\"file\"  (change)=\"onFileChanged(FileUploader)\" ng2FileSelect [uploader]=\"FileUploader\" multiple #fileInput/>\r\n<!-- <input (change)=\"onFileChanged($event)\"> -->\r\n<button mat-raised-button color=\"primary\" (click)=\"fileInput.click()\">Select File</button>\r\n",
-                styles: [""]
+                template: "<input  style=\"display: none\" type=\"file\"  (change)=\"onFileChanged(FileUploader)\" ng2FileSelect [uploader]=\"FileUploader\" multiple #fileInput/>\r\n<!-- <input (change)=\"onFileChanged($event)\"> -->\r\n<button mat-raised-button class=\"button\" (click)=\"fileInput.click()\">Select File(s)</button>\r\n",
+                styles: [".button{background-color:#3f51b5;color:#fff;border-color:#3f51b5;border-radius:4px;font-family:Arial;font-size:13.3333px;padding:6px;box-shadow:0 3px 1px -2px rgba(0,0,0,.2),0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12)!important}"]
             }] }
 ];
 /** @nocollapse */
